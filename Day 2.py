494d796eb9a8a97eef1ec10663f2b10c5ea1dd6c
naijacyberguy <worldnobleman0@gmail.com>
2025-04-11 20:31:29 -0600

    DAY 2
    Today I completed
    
    variables
    python data types
    Operators and operands
    Literals and Functions
    Coding exercises

---
+++ Day 2.py
@@ -0,0 +1,81 @@
+#this is how to store strings witihin a variable
+#number data type- stores numerical values, its value can be changed anytime but just changing its value
+x=10
+#string data type
+name="NCG"
+a=b=c=5
+print("x value",x)
+#type shows the class of whats chosen
+print("x-type:", type(x))
+print("name value:", name)
+print("name-type:", type(name))
+print("a value:", a)
+print("b value:", b)
+print("c value:", c)
+
+a=100
+b=300
+print(a)
+print(b)
+a=500
+b=900
+print(a)
+
+
+#this is how to store strings witihin a variable
+#number data type- stores numerical values, its value can be changed anytime but just changing its value
+x=10
+#string data type
+name="NCG"
+a=b=c=5
+print("x value",x)
+#type shows the class of whats chosen
+print("x-type:", type(x))
+print("name value:", name)
+print("name-type:", type(name))
+print("a value:", a)
+print("b value:", b)
+print("c value:", c)
+
+a=100
+print(a)
+a=500
+print(a)
+
+a=100 #a is a variable
+b=200 #b is a variable
+c=a+b #c is a variable
+        #c indicates an expression
+        #collection of operators and operands
+        #c --> operand
+        #a --.operand
+        #b -->operand
+        #+ -->operator
+        #= -->opeator
+c=100,200 #-->parameters
+#passing a value to an operand is called parameter.
+
+
+#DATA TYPES
+x=10
+print (type(x))
+x=0.654
+print(type(x))
+y=complex(1,2)
+print(y)
+
+#Binary Literals and bin function(base of 2)
+x=bin(64)
+print(x)
+
+#Octal Literals and oct function(base of 8)
+a=oct(8)
+print(a)
+b=0o10
+print(b)
+
+#Hexa-Decimal Literals and hex function(base of 16)
+x=hex(19)
+print(x)
+
+Order of operations
